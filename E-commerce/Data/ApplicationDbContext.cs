8f6cfbbd9b9a2d24881f257c30c83b3d60093710
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2024-11-15 13:48:43 +0200

    Add project files.

---
+++ E-commerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using E_commerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerce.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=E-Commerce509;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ E-commerce/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace E_commerce.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Product> Categories { get; } = new List<Product>();
+    }
+}
