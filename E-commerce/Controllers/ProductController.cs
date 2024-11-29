4bd297d6b0cd63e520219930d8d73cff670cd148
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2024-11-29 13:42:42 +0200

    Handle Img on Product Model

--- E-commerce/Controllers/ProductController.cs
+++ E-commerce/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using E_commerce.Data;
 using E_commerce.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,23 +13,43 @@ namespace E_commerce.Controllers
         public IActionResult Index()
         {
             var products = _dbContext.Products.Include(e=>e.Category).ToList();
-
+            ViewData["message"] = Request.Cookies["message"];
             return View(products);
         }
 
         public IActionResult Create()
         {
             var categories = _dbContext.Categories.ToList();
-            ViewBag.Categories = categories;
+            ViewData["categories"] = categories;
 
-            return View(categories);
+            return View();
         }
 
         [HttpPost]
-        public IActionResult Create(Product product)
+        public IActionResult Create(Product product, IFormFile file)
         {
+            if (file != null && file.Length > 0)
+            {
+                // Genereate name
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                // Save in wwwroot
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images", fileName);
+
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    file.CopyTo(stream);
+                }
+
+                // Save in db
+                product.Img = fileName;
+            }
+
             _dbContext.Products.Add(product);
             _dbContext.SaveChanges();
+
+            //Response.Cookies.Append("message", "Add product successfuly");
+            TempData["message"] = "Add product successfuly";
 
             return RedirectToAction("Index");
         }
@@ -40,16 +61,61 @@ namespace E_commerce.Controllers
             if (product == null) return RedirectToAction("NotFoundPage", "Home");
 
             var categories = _dbContext.Categories.ToList();
+
             ViewData["categories"] = categories;
 
             return View(product);
         }
 
         [HttpPost]
-        public IActionResult Edit(Product product)
+        public IActionResult Edit(Product product, IFormFile file)
         {
+            var oldProduct = _dbContext.Products.Where(e=>e.Id == product.Id).AsNoTracking().FirstOrDefault();
+            if (file != null && file.Length > 0)
+            {
+                // Genereate name
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                // Save in wwwroot
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images", fileName);
+
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    file.CopyTo(stream);
+                }
+
+                // Delete old img
+                var oldPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images", oldProduct.Img);
+                if (System.IO.File.Exists(oldPath))
+                {
+                    System.IO.File.Delete(oldPath);
+                }
+                
+                product.Img = fileName;
+            }
+            else
+            {
+                product.Img = oldProduct.Img;
+            }
+
             _dbContext.Products.Update(product);
             _dbContext.SaveChanges();
+
+            TempData["message"] = "Update product successfuly";
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int productId)
+        {
+            var product = _dbContext.Products.Find(productId);
+
+            if (product == null) return RedirectToAction("NotFoundPage", "Home");
+
+            _dbContext.Products.Remove(product);
+            _dbContext.SaveChanges();
+
+            TempData["message"] = "Delete product successfuly";
 
             return RedirectToAction("Index");
         }
